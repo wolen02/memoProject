871ce4505d21a8affdf3eb43d5d3032733955ff0
주지성\wolen <wnwltjd333@naver.com>
2023-02-09 22:25:17 +0900

    [주지성] - 최초파일 추가

---
+++ src/main/java/com/wolen/memo/config/DatabaseConfig.java
@@ -0,0 +1,34 @@
+package com.wolen.memo.config;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(basePackages="com.wolen.memo.*")
+public class DatabaseConfig {
+
+	@Bean
+	public SqlSessionFactory	sqlSessionFactory(DataSource dataSource) throws Exception  {
+		
+		
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+		
+		
+	}
+	
+}
